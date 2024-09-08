aa8c47b2534491825f6e9f5628daa38903da72f1
gurshaan17 <gurshaansinghd@gmail.com>
2024-09-08 22:25:09 +0530

    cors updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('./config/passport');
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'https://blog-gurshaan.vercel.app'],
     credentials: true 
 };
 
